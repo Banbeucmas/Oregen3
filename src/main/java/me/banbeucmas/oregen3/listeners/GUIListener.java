028f89b044a34f141afcd7400c39d2b093d62bca
xHexed <minhquanpct7b@gmail.com>
2020-04-06 22:21:11 +0700

    Using 1.13+ supported method

--- src/main/java/me/banbeucmas/oregen3/listeners/GUIListener.java
+++ src/main/java/me/banbeucmas/oregen3/listeners/GUIListener.java
@@ -7,7 +7,6 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class GUIListener implements Listener {
@@ -16,8 +15,7 @@ public class GUIListener implements Listener {
     @EventHandler
     public void onClick(final InventoryClickEvent e) {
         final ItemStack item = e.getCurrentItem();
-        final Inventory inv = e.getView().getTopInventory();
-        if (!inv.getTitle().equals(StringUtils.getColoredString(config.getString("messages.gui.title"))) ||
+        if (!e.getView().getTitle().equals(StringUtils.getColoredString(config.getString("messages.gui.title"))) ||
                 item == null ||
                 item.getType() == Material.AIR) {
             return;
