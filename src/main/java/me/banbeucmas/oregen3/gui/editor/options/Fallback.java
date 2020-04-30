01361b65785386267d9f4e2c920be6ec8aa545f5
xHexed <minhquanpct7b@gmail.com>
2020-04-30 21:01:51 +0700

    Gui update #3

--- src/main/java/me/banbeucmas/oregen3/gui/editor/options/Fallback.java
+++ src/main/java/me/banbeucmas/oregen3/gui/editor/options/Fallback.java
@@ -3,10 +3,15 @@ package me.banbeucmas.oregen3.gui.editor.options;
 import me.banbeucmas.oregen3.data.MaterialChooser;
 import me.banbeucmas.oregen3.gui.InventoryHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 public class Fallback implements InventoryHolder, InventoryHandler {
     private final Inventory inventory;
@@ -14,6 +19,13 @@ public class Fallback implements InventoryHolder, InventoryHandler {
     public Fallback(final MaterialChooser chooser) {
         inventory = Bukkit.createInventory(this, InventoryType.HOPPER, "Edit fallback block (" + chooser.getId() + ')');
 
+        final ItemStack item = new ItemStack(Material.PAPER);
+        final ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName("§rSet fallback item");
+        itemMeta.setLore(Arrays.asList("§rPut the item on the first slot (the left of this paper) to set the block,", "or click the required item in your inventory to set the item."));
+        item.setItemMeta(itemMeta);
+
+        inventory.setItem(1, item);
     }
 
     @Override
--- src/main/java/me/banbeucmas/oregen3/gui/editor/options/RandomBlockList.java
+++ src/main/java/me/banbeucmas/oregen3/gui/editor/options/RandomBlockList.java
@@ -7,12 +7,27 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
 
 public class RandomBlockList implements InventoryHolder, InventoryHandler {
     private final Inventory inventory;
 
     public RandomBlockList(final MaterialChooser chooser) {
         inventory = Bukkit.createInventory(this, 9, "Edit random blocks (" + chooser.getId() + ')');
+        final DecimalFormat format = new DecimalFormat("#.##");
+        chooser.getChances().forEach((mat, chance) -> {
+            final ItemStack item = new ItemStack(mat);
+            final ItemMeta itemMeta = item.getItemMeta();
+            itemMeta.setDisplayName(mat.name());
+            itemMeta.setLore(Collections.singletonList(format.format(chance)));
+            item.setItemMeta(itemMeta);
+
+            inventory.addItem(item);
+        });
     }
 
     @Override
