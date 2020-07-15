3fc19323b2c5e26b7535b8c557195f8f1d90e2bd
xHexed <minhquanpct7b@gmail.com>
2020-07-15 15:15:21 +0800

    Update BentoBoxHook

--- src/main/java/me/banbeucmas/oregen3/hooks/BentoBoxHook.java
+++ src/main/java/me/banbeucmas/oregen3/hooks/BentoBoxHook.java
@@ -2,6 +2,7 @@ package me.banbeucmas.oregen3.hooks;
 
 import me.banbeucmas.oregen3.Oregen3;
 import me.banbeucmas.oregen3.utils.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import world.bentobox.bentobox.BentoBox;
@@ -12,46 +13,46 @@ import world.bentobox.bentobox.database.objects.Island;
 import world.bentobox.bentobox.managers.AddonsManager;
 import world.bentobox.bentobox.managers.IslandsManager;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class BentoBoxHook implements SkyblockHook {
-    private World world;
+    private final Collection<World> worlds = new ArrayList<>();
     private GameModeAddon addon;
-    private final IslandsManager manager;
+    private IslandsManager manager;
     private boolean level = true;
 
     public BentoBoxHook() {
-        manager = BentoBox.getInstance().getIslands();
+        Bukkit.getScheduler().runTaskLater(Oregen3.getPlugin(), () -> {
+            manager = BentoBox.getInstance().getIslands();
 
-        if (!BentoBox.getInstance().getAddonsManager().getAddonByName("Level").isPresent()) {
-            Oregen3.getPlugin().getLogger().warning(StringUtils.getPrefixString("Level addon for BentoBox not found! Turning island level feature off...", null));
-            level = false;
-        }
-
-        final AddonsManager manager = BentoBox.getInstance().getAddonsManager();
-        final List<GameModeAddon> addonList = manager.getGameModeAddons();
-        if (addonList.isEmpty()) {
-            Oregen3.getPlugin().getLogger().severe(StringUtils.getPrefixString("Couldn't find a gamemode for BentoBox! Disabling plugin...", null));
-            Oregen3.getPlugin().getPluginLoader().disablePlugin(Oregen3.getPlugin());
-            return;
-        }
-
-        for (final String name : Oregen3.getPlugin().getConfig().getStringList(StringUtils.getPrefixString("hooks.BentoBox.gamemodePriorities", null))) {
-            final Optional<Addon> gamemode = manager.getAddonByName(name);
-            if (!gamemode.isPresent() || !(gamemode.get() instanceof GameModeAddon)) continue;
-            final GameModeAddon gamemodeAddon = (GameModeAddon) gamemode.get();
-            if (addonList.contains(gamemodeAddon)) {
-                addon = gamemodeAddon;
+            if (!BentoBox.getInstance().getAddonsManager().getAddonByName("Level").isPresent()) {
+                Oregen3.getPlugin().getLogger().warning(StringUtils.getPrefixString("Level addon for BentoBox not found! Turning island level feature off...", null));
+                level = false;
             }
-        }
-        if (addon == null) {
-            Oregen3.getPlugin().getServer().getConsoleSender().sendMessage(StringUtils.getPrefixString("Couldn't find any gamemode hook list in config, trying to grab the first one it can find...", null));
-            addon = addonList.get(0);
-        }
-        world = addon.getOverWorld();
+
+            final AddonsManager manager = BentoBox.getInstance().getAddonsManager();
+            final List<GameModeAddon> addonList = manager.getGameModeAddons();
+            if (addonList.isEmpty()) {
+                Oregen3.getPlugin().getLogger().severe(StringUtils.getPrefixString("Couldn't find a gamemode for BentoBox! Disabling plugin...", null));
+                Oregen3.getPlugin().getPluginLoader().disablePlugin(Oregen3.getPlugin());
+                return;
+            }
+
+            for (final String name : Oregen3.getPlugin().getConfig().getStringList("hooks.BentoBox.gamemodePriorities")) {
+                final Optional<Addon> gamemode = manager.getAddonByName(name);
+                if (!gamemode.isPresent() || !(gamemode.get() instanceof GameModeAddon)) continue;
+                final GameModeAddon gamemodeAddon = (GameModeAddon) gamemode.get();
+                if (addonList.contains(gamemodeAddon)) {
+                    addon = gamemodeAddon;
+                    worlds.add(addon.getOverWorld());
+                }
+            }
+
+            if (addon == null) {
+                Oregen3.getPlugin().getServer().getConsoleSender().sendMessage(StringUtils.getPrefixString("Couldn't find any gamemode hook list in config, trying to grab the first one it can find...", null));
+                addon = addonList.get(0);
+            }
+        }, 2);
     }
 
     @Override
@@ -68,9 +69,12 @@ public class BentoBoxHook implements SkyblockHook {
                         .request();
             }
             if (num == 0) {
-                num = (double) builder
-                        .addMetaData("world-name", world)
-                        .request();
+                for (final World world : worlds) {
+                    num = (double) builder
+                            .addMetaData("world-name", world.getName())
+                            .request();
+                    if (num != 0) break;
+                }
             }
             return num;
         }
@@ -86,13 +90,21 @@ public class BentoBoxHook implements SkyblockHook {
 
     @Override
     public UUID getIslandOwner(final UUID uuid) {
-        return manager.getOwner(world, uuid);
+        for (final World world : worlds) {
+            final UUID owner = manager.getOwner(world, uuid);
+            if (owner == null) continue;
+            return owner;
+        }
+        return null;
     }
 
     @Override
     public List<UUID> getMembers(final UUID uuid) {
-        final Island island = manager.getIsland(world, uuid);
-        if (island == null) return null;
-        return new ArrayList<>(island.getMembers().keySet());
+        for (final World world : worlds) {
+            final Island island = manager.getIsland(world, uuid);
+            if (island == null) continue;
+            return new ArrayList<>(island.getMembers().keySet());
+        }
+        return null;
     }
 }
