6dda2c4d41fb4ae743477c0f926e59904db2bb66
xHexed <minhquanpct7b@gmail.com>
2020-07-15 20:46:16 +0800

    Fix help command

--- src/main/java/me/banbeucmas/oregen3/commands/HelpCommand.java
+++ src/main/java/me/banbeucmas/oregen3/commands/HelpCommand.java
@@ -26,9 +26,6 @@ public class HelpCommand extends AbstractCommand {
         if (sender.hasPermission("oregen3.information")) {
             sender.sendMessage(StringUtils.getColoredPrefixString(LABEL.matcher(Oregen3.getPlugin().getConfig().getString("messages.commands.info")).replaceAll(getLabel()), getPlayer()));
         }
-        if (sender.hasPermission("oregen3.debug")) {
-            sender.sendMessage(StringUtils.getColoredPrefixString(LABEL.matcher(Oregen3.getPlugin().getConfig().getString("messages.commands.debug")).replaceAll(getLabel()), getPlayer()));
-        }
         if (sender.hasPermission("oregen3.edit")) {
             sender.sendMessage(StringUtils.getColoredPrefixString(LABEL.matcher(Oregen3.getPlugin().getConfig().getString("messages.commands.edit")).replaceAll(getLabel()), getPlayer()));
         }
